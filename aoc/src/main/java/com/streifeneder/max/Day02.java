eff7bb3dde978a1a070c87a09e9a36e4ef0b1fbe
Maxi Streifeneder <maximilian.streifeneder@sap.com>
2019-12-03 15:48:14 +0100

    Day 2 correction

--- aoc/src/main/java/com/streifeneder/max/Day02.java
+++ aoc/src/main/java/com/streifeneder/max/Day02.java
@@ -17,11 +17,12 @@ public class Day02 {
     private static final int TARGET = 19690720;
 
     public static void main(String[] args) throws IOException {
-        int[] originalNums = Stream.of(AdventOfCodeHelper.getFileAsString().split(",")).mapToInt(Integer::valueOf).toArray();
+        int[] originalNums = Stream.of(AdventOfCodeHelper.getFileAsString().split(",")).mapToInt(Integer::valueOf)
+                .toArray();
         int[] part1Nums = Arrays.copyOf(originalNums, originalNums.length);
         int[] part2Nums = Arrays.copyOf(originalNums, originalNums.length);
         System.out.printf("Part1 solution is %d %n", solveDay1(part1Nums));
-        System.out.printf("Part2 solution is %s %n", solveDay2(part2Nums));
+        System.out.printf("Part2 solution is %d %n", solveDay2(part2Nums));
     }
 
     private static int solveDay1(int[] originalNums) {
@@ -29,21 +30,21 @@ public class Day02 {
         originalNums[2] = 2;
         return runIntProgram(originalNums);
     }
-    
-    private static String solveDay2(int[] originalNums) {
-        for (int noun=0;noun < 99;noun++) {
-            for (int verb=0;verb < 99;verb++) {
+
+    private static int solveDay2(int[] originalNums) {
+        for (int noun = 0; noun < 99; noun++) {
+            for (int verb = 0; verb < 99; verb++) {
                 int[] initNums = Arrays.copyOf(originalNums, originalNums.length);
                 initNums[1] = noun;
                 initNums[2] = verb;
                 if (runIntProgram(initNums) == TARGET)
-                    return String.format("%02d", noun) + String.format("%02d", verb);            
-            }         
+                    return 100 * noun + verb;
+            }
         }
-        return "hmpf!"; 
-	}
+        return 0;
+    }
 
-	public static int runIntProgram(int[] numbers) {
+    public static int runIntProgram(int[] numbers) {
         int position = 0;
         int opcode = Integer.valueOf(numbers[position]);
 
